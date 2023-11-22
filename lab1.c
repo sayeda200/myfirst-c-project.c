fe7e2c40a4537d6cfb4a11ce7fa09b5b12fca0e2
sayeda yasmeen <149186687+sayeda200@users.noreply.github.com>
2023-11-22 02:57:37 -0800

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+int a,b;
+printf("enter the value of a&b\n");
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+       printf("a is big\n");
+}
+else
+{
+       printf("b is big\n");
+}
+}
