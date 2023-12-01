452c02b400e2131cdcdbadd37e40c435061d92e9
sayeda yasmeen <149186687+sayeda200@users.noreply.github.com>
2023-12-01 02:32:33 -0800

    Basic arithmetic operations

---
+++ Basic arithmetic operations.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f,g;
+printf("enter the value of a&b\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+g=a%b;
+printf("sum of a&b=%d",c);
+printf("sub of a&b=%d",d);
+printf("multi of a&b=%d",e);
+printf("div of a&b=%d",f);
+printf("mod of a&b=%d",g);
+}
